36a3e4e565634167cd125a623a3ac1f6aba6fc89
VIKASH BHARTIA <VB.NET20010@GMAIL.COM>
2021-12-24 23:02:55 +0530

    commit payment server changes

---
+++ server.js
@@ -0,0 +1,45 @@
+// This is a public sample test API key.
+// To avoid exposing it, don't submit any personally identifiable information through requests with this API key.
+// Sign in to see your own test API key embedded in code samples.
+const stripe = require('stripe')('sk_test_cnyRPIzV7vSWphzJnxvH7xed');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded());
+
+
+
+app.post('/create-checkout-session', async (req, res) => {
+
+  
+  const YOUR_DOMAIN = req.headers.referer;
+  const session = await stripe.checkout.sessions.create({
+    customer_email: req.body.customer_email,
+    submit_type: 'donate',
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: req.body.itemName,
+            images:[req.body.itemImage]
+          },
+          unit_amount: req.body.unitPrice*100,
+        },
+        quantity: req.body.qty,
+        description: "spring fundraiser 2022"
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
